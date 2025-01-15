d3edcb77344f5b7ecf6ef7a48b8f11bfc148ec0e
NikoDood <162916390+NikoDood@users.noreply.github.com>
2025-01-15 16:18:40 +0100

    make SLA page

---
+++ Pages/SLA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorWhoknowsV2.Pages
+{
+    public class SLAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
